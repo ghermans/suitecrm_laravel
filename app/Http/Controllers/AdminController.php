0b4ec8bab9034fea84a3c77c5633911ab7a21475
Glenn Hermans <glhermans@gmail.com>
2015-11-10 21:08:09 +0100

    Added mail settings

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -28,12 +28,16 @@ class AdminController extends Controller
       return view('admin.connector', compact('url', 'username'));
     }
 
-
     public function store_connector(Request $request, ConnectorValidator $input)
     {
       // This needs to be written into a middleware.
        \Config::set('suitecrm.url', $request->get('hostname'));
        \Config::set('suitecrm.url', $request->get('username'));
        \Config::set('suitecrm.password', $request->get('password'));
+    }
+
+    public function mail()
+    {
+      return view('admin.list_mail');
     }
 }
