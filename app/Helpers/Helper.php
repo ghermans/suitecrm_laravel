f32b194fdcb4da606acb66a116ac67ceb1b54524
Glenn Hermans <glhermans@gmail.com>
2015-11-14 20:25:20 +0100

    Added new helper file for #99

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Helpers
+
+class Helper {
+
+
+/**
+ * Converts an Array to a SugarCRM-REST compatible name_value_list
+ *
+ * @param Array $data
+ * @return Array
+*/
+
+ function convertArrayToNVL($data)
+  {
+	  $return = array();
+	  foreach ( $data as $key => $value )
+	 	$return[] = array( 'name' => $key, 'value' => $value );
+	  return $return;
+  }
+
+    /**
+     * Converts a SugarCRM-REST compatible name_value_list to an Array
+     *
+     * @param Array $data
+     * @return Array
+     */
+
+  function convertNVLToArray ($data ){
+    $return = array();
+    foreach ($data as $row ){
+    	$return[$row['name']] = $row['value'];
+      }
+    return $return;
+    }
+ }
+?>
