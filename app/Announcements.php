ae7c10fab065cdafbcc8ac4bf46f519796666814
Tjoosten <Tjoosten3@gmail.com>
2016-02-25 23:44:42 +0100

    Add missing to docblocks to the protected $table -> models

--- app/Announcements.php
+++ app/Announcements.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Announcements extends Model
 {
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
   protected $table = 'announcements';
 }
